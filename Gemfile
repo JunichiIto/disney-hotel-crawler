82c796bca5320d803cfed66e386a6f76dc115fd6
Junichi Ito <jit@sonicgarden.jp>
2019-08-24 14:43:17 +0900

    Use config to hide real mail from/to

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mechanize'
+gem 'config'
--- app/mailers/hotel_plan_mailer.rb
+++ app/mailers/hotel_plan_mailer.rb
@@ -2,8 +2,8 @@ class HotelPlanMailer < ApplicationMailer
   def notification(hotel_plans)
     @hotel_plans = hotel_plans
     subject = '新しいプランが見つかりました'
-    to = 'jnchito@example.com'
-    from = 'noreply@example.com'
+    to = Settings.mail.to
+    from = Settings.mail.from
     mail(to: to, from: from, subject: subject)
   end
 end
